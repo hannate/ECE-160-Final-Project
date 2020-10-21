4002d31f7ec07cc90cfde5e72bbb84605bc87a43
hannate <71795519+hannate@users.noreply.github.com>
2020-10-21 19:31:00 -0400

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,25 +1,36 @@
 #include <PS2X_lib.h>  //for v1.6
 #include <Servo.h>
-
+#include <SPI.h>
+#include <NRFLite.h>
 
 PS2X ps2x; // create PS2 Controller Class
 
-//right now, the library does NOT support hot pluggable controllers, meaning
-//you must always either restart your Arduino after you conect the controller,
-//or call config_gamepad(pins) again after connecting the controller.
 int error = 0;
 byte type = 0;
 byte vibrate = 0;
 Servo wheelLeft;
 Servo wheelRight;
 Servo gripper;
+const static uint8_t RADIO_ID = 0;
+const static uint8_t PIN_RADIO_CE = 9;
+const static uint8_t PIN_RADIO_CSN = 10;
 bool joystickMode = false;
 int gripperPosition = 0;
+
+struct RadioPacket { //max 32 bytes
+  uint8_t FromRadioId;
+  uint32_t OnTimeMillis;
+  uint32_t FailedTxCount;
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+
 void setup() {
   Serial.begin(9600);
-  wheelLeft.attach(13);
-  wheelRight.attach(12);
-  gripper.attach(11);
+  wheelLeft.attach(4);
+  wheelRight.attach(3);
+  gripper.attach(2);
 
   //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
 
@@ -56,6 +67,10 @@ void setup() {
       break;
   }
 
+  if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN)) {
+    Serial.println("Cannot communicate with transciever");
+  }
+
 }
 
 void loop() {
@@ -74,6 +89,7 @@ void loop() {
     //Change between button mode and joystick mode
     if (ps2x.ButtonPressed(PSB_SELECT)) {
       joystickMode = !joystickMode;
+      Serial.println("Changing control scheme");
     }
 
     //Move robot based on selected control scheme
@@ -92,66 +108,81 @@ void loop() {
       }
     }
     gripper.write(gripperPosition);
-    
+
   }
 
-
+  readTransceiver();
+  
   delay(50);
 
 }
 
 void joystickControls() {
   int leftStickYAxis = ps2x.Analog(PSS_LY);
-      int leftStickXAxis = ps2x.Analog(PSS_LX);
-      int speedLeft = map(leftStickYAxis, 0, 255, 135, 45);
-      Serial.print(speedLeft);
-      Serial.print(" ");
-      speedLeft = speedLeft + map(leftStickXAxis, 0, 255, -45, 45);
-      int speedRight = map(leftStickYAxis, 0, 255, 45, 135);
-      Serial.print(speedRight);
-      Serial.print(" ");
-      speedRight = speedRight + map(leftStickXAxis, 0, 255, -45, 45);
-      Serial.print(speedLeft);
-      Serial.print(" ");
-      Serial.println(speedRight);
-      if (speedLeft <= 95 && speedLeft >= 85 && speedRight <= 95 && speedRight >= 85) {
-        wheelLeft.writeMicroseconds(1500);
-        wheelRight.writeMicroseconds(1500);
-      } else {
-        wheelLeft.write(speedLeft);
-        wheelRight.write(speedRight);
-      }
+  int leftStickXAxis = ps2x.Analog(PSS_LX);
+  int speedLeft = map(leftStickYAxis, 0, 255, 135, 45);
+  Serial.print(speedLeft);
+  Serial.print(" ");
+  speedLeft = speedLeft + map(leftStickXAxis, 0, 255, -45, 45);
+  int speedRight = map(leftStickYAxis, 0, 255, 45, 135);
+  Serial.print(speedRight);
+  Serial.print(" ");
+  speedRight = speedRight + map(leftStickXAxis, 0, 255, -45, 45);
+  Serial.print(speedLeft);
+  Serial.print(" ");
+  Serial.println(speedRight);
+  if (speedLeft <= 95 && speedLeft >= 85 && speedRight <= 95 && speedRight >= 85) {
+    wheelLeft.writeMicroseconds(1500);
+    wheelRight.writeMicroseconds(1500);
+  } else {
+    wheelLeft.write(speedLeft);
+    wheelRight.write(speedRight);
+  }
 }
 
 void buttonControls() {
   if (ps2x.Button(PSB_PAD_UP)) {        //will be TRUE as long as button is pressed
-        Serial.print("Up held this hard: ");
-        Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
-        wheelLeft.write(135);
-        wheelRight.write(45);
-      } else if (ps2x.Button(PSB_PAD_RIGHT)) {
-        Serial.print("Right held this hard: ");
-        Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-        wheelLeft.write(135);
-        wheelRight.write(90);
-      } else if (ps2x.Button(PSB_PAD_LEFT)) {
-        Serial.print("LEFT held this hard: ");
-        Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-        wheelLeft.write(95);
-        wheelRight.write(45);
-      } else if (ps2x.Button(PSB_PAD_DOWN)) {
-        Serial.print("DOWN held this hard: ");
-        Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-        wheelLeft.write(45);
-        wheelRight.write(135);
-      } else if (ps2x.Button(PSB_L2)) {
-        wheelLeft.write(45);
-        wheelRight.write(45);
-      }  else if (ps2x.Button(PSB_R2)) {
-        wheelLeft.write(135);
-        wheelRight.write(135);
-      } else {
-        wheelLeft.writeMicroseconds(1500);
-        wheelRight.writeMicroseconds(1500);
-      }
+    Serial.print("Up held this hard: ");
+    Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
+    wheelLeft.write(135);
+    wheelRight.write(45);
+  } else if (ps2x.Button(PSB_PAD_RIGHT)) {
+    Serial.print("Right held this hard: ");
+    Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
+    wheelLeft.write(135);
+    wheelRight.write(90);
+  } else if (ps2x.Button(PSB_PAD_LEFT)) {
+    Serial.print("LEFT held this hard: ");
+    Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
+    wheelLeft.write(95);
+    wheelRight.write(45);
+  } else if (ps2x.Button(PSB_PAD_DOWN)) {
+    Serial.print("DOWN held this hard: ");
+    Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
+    wheelLeft.write(45);
+    wheelRight.write(135);
+  } else if (ps2x.Button(PSB_L2)) {
+    wheelLeft.write(45);
+    wheelRight.write(45);
+  }  else if (ps2x.Button(PSB_R2)) {
+    wheelLeft.write(135);
+    wheelRight.write(135);
+  } else {
+    wheelLeft.writeMicroseconds(1500);
+    wheelRight.writeMicroseconds(1500);
+  }
+}
+
+void readTransceiver() {
+  _radio.readData(&_radioData); 
+
+  String msg = "Radio ";
+  msg += _radioData.FromRadioId;
+  msg += ", ";
+  msg += _radioData.OnTimeMillis;
+  msg += " ms, ";
+  msg += _radioData.FailedTxCount;
+  msg += " Failed TX";
+
+  Serial.println(msg);
 }
